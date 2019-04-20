2de0238c8d469304fdb3f43853709fdd81b04a8a
AjayKumarTyagi <46739062+AjayKumarTyagi@users.noreply.github.com>
2019-04-20 21:49:28 +0530

    Delete record functionality added

--- LMS.cpp
+++ LMS.cpp
@@ -387,6 +387,54 @@ void modify_student()
 	getch();
 
 }
+void delete_student()
+{
+	char n[6];
+
+	int flag=0;
+	
+       system("cls");
+
+    	cout<<"\n\n\n\tDELETE STUDENT...";
+
+    	cout<<"\n\nEnter The admission no. of the Student You Want To Delete : ";
+
+    	cin>>n;
+
+    	fp.open("student.dat",ios::in|ios::out);
+
+    	fstream fp2;
+
+    	fp2.open("Temp.dat",ios::out);
+
+    	fp.seekg(0,ios::beg);
+
+    	while(fp.read((char*)&st,sizeof(student)))
+	{
+		if(strcmpi(st.retadmno(),n)!=0)
+
+	     		fp2.write((char*)&st,sizeof(student));
+
+		     else
+
+	   		flag=1;
+	}
+    	
+	fp2.close();
+
+    	fp.close();
+
+    	remove("student.dat");
+
+    	rename("Temp.dat","student.dat");
+
+    	if(flag==1)
+     		cout<<"\n\n\tRecord Deleted ..";
+    	else
+     		cout<<"\n\nRecord not found";
+
+    	getch();
+}
 
 
 
