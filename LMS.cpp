fbaf9a1dcbe362fc76a5d7a0b80ad179b46987ec
Aayush Kumar <you@example.com>
2019-04-20 20:02:26 +0530

    created class book with all its functions

---
+++ LMS.cpp
@@ -0,0 +1,80 @@
+
+#include<fstream>
+
+#include<stdlib.h>
+
+#include<string.h>
+
+#include<iostream>
+
+using namespace std;
+
+
+class book
+{
+	char bno[6];
+	
+	char bname[50];
+	
+	char aname[20];
+	
+  public:
+  	
+	void create_book()
+	{
+		cout<<"\nNEW BOOK ENTRY...\n";
+		
+		cout<<"\nEnter The book no.";
+		
+		cin>>bno;
+		
+		cout<<"\n\nEnter The Name of The Book ";
+		
+		gets(bname);
+		
+		cout<<"\n\nEnter The Author's Name ";
+		
+		gets(aname);
+		
+		cout<<"\n\n\nBook Created..";
+	}
+
+	void show_book()
+	{
+		cout<<"\nBook no. : "<<bno;
+		
+		cout<<"\nBook Name : ";
+		
+		puts(bname);
+		
+		cout<<"Author Name : ";
+		
+		puts(aname);
+	}
+
+	void modify_book()
+	{
+		cout<<"\nBook no. : "<<bno;
+		
+		cout<<"\nModify Book Name : ";
+		
+		gets(bname);
+		
+		cout<<"\nModify Author's Name of Book : ";
+		
+		gets(aname);
+	}
+
+	char* retbno()
+	{
+		return bno;
+	}
+
+	void report()
+	{
+	cout<<bno<<setw(30)<<bname<<setw(30)<<aname<<endl;
+	}
+
+
+};         //class ends here
+
